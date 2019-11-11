10274768d8d13b027895abd80a801bc49242207f
MkHunter <miguelernesto_23@hotmail.com>
2019-11-11 16:52:50 -0700

    Servidor NodeJS - Configurado

---
+++ node/crud-mysql-ejemplo/src/app.js
@@ -0,0 +1,25 @@
+//Código del servidor
+const path = require('path');
+const express = require('express');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
+const app = express();
+
+//Importar rutas
+const indiceRutas = require('./rutas/index');
+
+//Configuraciones
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'vistas'));
+
+//Middleware
+//  app.use()
+
+//Usamos las rutas
+app.use('/', indiceRutas);
+
+app.listen(app.get('port'),() =>{
+    console.log('Escuchando en el puerto 3000');
+});
+
